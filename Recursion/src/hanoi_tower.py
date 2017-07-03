229f58f4fa387642be2c6a8d3a6af197a20786ae
Cherish <cherish@flotechnologies.com>
2017-07-03 11:32:08 -0700

    Add a recursive solution and tests for Hanoi Tower

--- Recursion/src/hanoi_tower.py
+++ Recursion/src/hanoi_tower.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
-def move_tower(height, src, dest, buf):
-    src[:], dest[:] = dest[:], src[:]
+def move_tower(h, s, d, b):
+    if h >= 1:
+        move_tower(h-1, s, b, d)
+        d.insert(0, s.pop(0))
+        move_tower(h-1, b, d, s)
